9ead1dd15b7732b77f4a75229da2df12270c204e
yousseftarhri <yousseftarhri@gmail.com>
2024-11-03 15:39:40 +0100

    clean code

--- code.py
+++ code.py
@@ -1,22 +1,24 @@
 def minion_game(string):
     vowel = 'AEIOU'
-    StuartScore = 0
-    KevinScore = 0
-    l = 0
+    StuartScore = KevinScore = 0
+
     l = len(string)
     for i in range(l):
         if string[i] not in vowel:
-            
             StuartScore += l - i
+
         else:
-            
             KevinScore += l - i
+
     if StuartScore > KevinScore:
         print("Stuart", StuartScore)
+
     elif StuartScore < KevinScore:
         print("Kevin", KevinScore)
+
     else:
-        print("Draw") 
+        print("Draw")
+
     return
         
 
